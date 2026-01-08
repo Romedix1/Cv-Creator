b0512a41a93063a3c4f9959290debd9485b7445f
Michał Dobosz <doboszmichal4@gmail.com>
2026-01-08 21:10:41 +0100

    fix: resolve WCAG issues

--- src/components/ui/HamburgerMenu.tsx
+++ src/components/ui/HamburgerMenu.tsx
@@ -1,4 +1,4 @@
-'use client'
+import { useTranslations } from "next-intl";
 
 type HamburgerMenuProps = {
     isOpen: boolean;
@@ -6,17 +6,19 @@ type HamburgerMenuProps = {
 }
 
 export default function HamburgerMenu({ isOpen, setIsOpen }: HamburgerMenuProps) {
+    const tAria = useTranslations("Aria")
+
     function handleClick() {
         setIsOpen((prev) => !prev)
     }
 
     return (
-        <button onClick={handleClick} className="flex flex-col justify-center items-center cursor-pointer">
-            <span className={`bg-bg-second block transition-all ease-out h-[3px] w-5 rounded-sm ${isOpen ? 'rotate-45 translate-y-[5px]' : '-translate-y-0.5'}`}>
+        <button aria-label={isOpen ? tAria("openNavMeu") : tAria("closeNavMenu")} onClick={handleClick} className="flex flex-col justify-center items-center cursor-pointer">
+            <span className={`bg-bg-second block transition-all ease-out h-0.75 w-5 rounded-sm ${isOpen ? 'rotate-45 translate-y-1.25' : '-translate-y-0.5'}`}>
             </span>
-            <span className={`bg-bg-second block transition-all ease-out h-[3px] w-5 rounded-sm my-0.5 ${isOpen ? 'opacity-0' : 'opacity-100'}`}>
+            <span className={`bg-bg-second block transition-all ease-out h-0.75 w-5 rounded-sm my-0.5 ${isOpen ? 'opacity-0' : 'opacity-100'}`}>
             </span>
-            <span className={`bg-bg-second block transition-all ease-out h-[3px] w-5 rounded-sm ${isOpen ? '-rotate-45 -translate-y-[5px]' : 'translate-y-0.5'}`}>
+            <span className={`bg-bg-second block transition-all ease-out h-0.75 w-5 rounded-sm ${isOpen ? '-rotate-45 -translate-y-1.25' : 'translate-y-0.5'}`}>
             </span>
         </button>
     )
--- src/components/ui/ThemeSwitcher.tsx
+++ src/components/ui/ThemeSwitcher.tsx
@@ -1,11 +1,12 @@
-'use client'
-
 import { useTheme } from "next-themes";
 import { SunIcon } from "../icons/SunIcon";
 import { MoonIcon } from "../icons/MoonIcon";
+import { useTranslations } from "next-intl";
 
 export default function ThemeSwitcher() {
-    const { theme, setTheme } = useTheme();
+    const tAria = useTranslations("Aria")
+
+    const { theme, setTheme, resolvedTheme } = useTheme();
 
     const toggleSwitch = () => {
         const newTheme = theme === 'dark' ? 'light' : 'dark';
@@ -13,10 +14,10 @@ export default function ThemeSwitcher() {
         document.cookie = `theme=${newTheme}; path=/; max-age=31536000; SameSite=Lax`;
     }
 
-    const isDark = theme === "dark";
+    const isDark = theme === "dark" || resolvedTheme === "dark"
 
     return (
-        <button onClick={toggleSwitch} className="w-15 h-9 bg-gray-300 dark:bg-default hover:dark:bg-default-hover hover:bg-[#BFBFBF] rounded-full p-1 flex items-center cursor-pointer transition-colors ">
+        <button aria-label={isDark ? tAria("switchToLight") : tAria("switchToDark")} onClick={toggleSwitch} className="w-15 h-9 bg-gray-300 dark:bg-default hover:dark:bg-default-hover hover:bg-[#BFBFBF] rounded-full p-1 flex items-center cursor-pointer transition-colors ">
             <div className={`w-7 h-7 rounded-full bg-surface-static shadow-[0px_2px_4px_0px_rgba(0,0,0,0.33)] flex items-center justify-center transition-transform ${isDark && "translate-x-6"}`}>
                 {!isDark ? (
                         <SunIcon className="w-5 h-5 transition-all"/>
