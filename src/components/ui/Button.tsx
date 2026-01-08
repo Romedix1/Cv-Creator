71e38b46eb6c8a8866891e6f2c3d3fba6424cc95
Michał Dobosz <doboszmichal4@gmail.com>
2026-01-08 20:05:07 +0100

    feature: pass selected template to cv builder

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { ReactNode } from "react";
 
 type ButtonProps = {
@@ -9,12 +10,21 @@ type ButtonProps = {
     text?: string;
     icon?: ReactNode;
     "aria-label"?: string;
+    href?: string;
 }
 
-export default function Button({ className, type="button", disabled, onClick, variant, text, icon, "aria-label": ariaLabel }: ButtonProps) {
+export default function Button({ className, type="button", disabled, onClick, variant, text, icon, "aria-label": ariaLabel, href }: ButtonProps) {
     const baseStyles = "flex justify-center items-center py-2.5 rounded-lg font-semibold cursor-pointer duration-200";
 
     const variantStyles = variant === "primary" ? "bg-default text-button-text hover:bg-default-hover" : variant === "secondary" ? "bg-transparent text-text-main border border-border hover:bg-surface-hover" : variant === "remove" ? "text-text-muted hover:text-error" : "text-text-muted hover:text-default"
+
+    if (href) {
+        return (
+            <Link href={href} className={`${className} ${baseStyles} ${variantStyles}`}>
+                {text}
+            </Link>
+        );
+    }
 
     return (
         <button type={type} disabled={disabled} onClick={onClick} aria-label={ariaLabel} className={`${className} ${baseStyles} ${variantStyles}`}>
