454ef10602d5745ac7733ae3cf33bce5d1e55c85
Michał Dobosz <doboszmichal4@gmail.com>
2026-02-12 14:24:43 +0100

    fix: template routing logic and preview modal UI

--- src/components/layout/HeroSection.tsx
+++ src/components/layout/HeroSection.tsx
@@ -2,12 +2,13 @@ import { useTranslations } from "next-intl"
 import Button from "../ui/Button";
 import CvPreview from '../../../public/images/Templates/modern-blue.png'
 import Image from "next/image";
-import Link from "next/link";
+import ScrollButton from "../ui/ScrollButton";
 
 export default function HeroSection() {
     const tHero = useTranslations("Hero");
     const tNav = useTranslations("Nav");
     const tButton = useTranslations("Button");
+    const tAlt = useTranslations("Alt");
 
     return (
         <section className="py-12 px-5 flex flex-col gap-16 items-center lg:flex-row lg:px-24 2xl:px-30">
@@ -18,18 +19,14 @@ export default function HeroSection() {
                 </div>
 
                 <div className="flex flex-col gap-4 lg:flex-row">
-                    <Link href={"/cv-builder"}>
-                        <Button className="w-full lg:w-fit lg:px-9 2xl:text-lg" variant="primary" text={tButton("createCV")} />
-                    </Link>
-                    <Link href={"/templates"}>
-                        <Button className="w-full lg:w-fit lg:px-9 2xl:text-lg" variant="secondary" text={tNav("templates")} />
-                    </Link>
+                    <Button className="w-full lg:w-fit lg:px-9 2xl:text-lg" variant="primary" text={tButton("createCV")} href="/templates"/>
+                    <ScrollButton text={tNav("templates")} />
                 </div>
             </div>
             <div>
-                <div className="aspect-3/4 relative w-[300px] 2xl:w-[400px]">
-                    <div className="absolute w-[300px] 2xl:w-[500px] h-[400px] 2xl:h-[500px] bg-default -left-[25px] rounded-full opacity-25 blur-[100px]"></div>
-                    <Image src={CvPreview} alt="CV preview" fill className="object-contain" />
+                <div className="aspect-3/4 relative w-75 2xl:w-[400px]">
+                    <div className="absolute w-75 2xl:w-[500px] h-[400px] 2xl:h-[500px] bg-default -left-[25px] rounded-full opacity-25 blur-[100px]"></div>
+                    <Image src={CvPreview} alt={tAlt("preview")} fill className="object-contain" />
                 </div>
             </div>
         </section>
