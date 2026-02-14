26bca0d4adfd04ad846ee0ef3103199b59873ce0
Michał Dobosz <doboszmichal4@gmail.com>
2026-02-15 00:07:01 +0100

    feature: add metadata to: Main page, Templates, Dashboard, Login and Editor

--- src/components/templates/CreativeAccent.tsx
+++ src/components/templates/CreativeAccent.tsx
@@ -369,7 +369,7 @@ export default function CreativeAccent({ data }: { data: ResumeData }) {
         <div className={cn("bg-white flex flex-col", inter.className, "w-full text-black")}>
             {/* TOP */}
             <div className="flex w-full py-11 relative" style={{ background: userColor }}>
-                <AppImage src={data.personalInfo.avatarUrl || ""} alt={tAlt("userImage")} width={400} height={532} quality={100} className="w-full h-full object-cover" containerClassName="rounded-full w-30 h-30 object-cover absolute ring-4 ring-white left-6 bottom-0 translate-y-1/2"/>
+                {data.personalInfo.avatarUrl && <AppImage src={data.personalInfo.avatarUrl} alt={tAlt("userImage")} width={400} height={532} quality={100} className="w-full h-full object-cover" containerClassName="rounded-full w-30 h-30 object-cover absolute ring-4 ring-white left-6 bottom-0 translate-y-1/2"/>}
 
                 {(data.personalInfo.firstName || data.personalInfo.lastName || data.personalInfo.jobTitle) &&
                     <div className="text-left w-full pl-42">
