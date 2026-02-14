91037c9cf70b52e7a3a69b48eaf1b2bbb2ff3901
Michał Dobosz <doboszmichal4@gmail.com>
2026-02-14 22:29:13 +0100

    fix: english translations

--- src/components/layout/HamburgerMenu.tsx
+++ src/components/layout/HamburgerMenu.tsx
@@ -14,7 +14,7 @@ export default function HamburgerMenu({ isOpen, setIsOpen }: HamburgerMenuProps)
     }
 
     return (
-        <button aria-label={isOpen ? tAria("openNavMeu") : tAria("closeNavMenu")} onClick={handleClick} className="flex flex-col justify-center items-center cursor-pointer">
+        <button aria-label={isOpen ? tAria("openNavMenu") : tAria("closeNavMenu")} onClick={handleClick} className="flex flex-col justify-center items-center cursor-pointer">
             <span className={cn("bg-bg-second block transition-all ease-out h-0.75 w-5 rounded-sm", isOpen ? 'rotate-45 translate-y-1.25' : '-translate-y-0.5')}>
             </span>
             <span className={cn("bg-bg-second block transition-all ease-out h-0.75 w-5 rounded-sm my-0.5", isOpen ? 'opacity-0' : 'opacity-100')}>
