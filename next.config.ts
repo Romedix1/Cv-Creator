1778ff6f3b9d78890504c69a27da875f1d642498
Michał Dobosz <doboszmichal4@gmail.com>
2026-02-15 13:23:26 +0100

    fix: add .npmrc

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: ['@sparticuz/chromium'],
-  experimental: {
-    serverComponentsExternalPackages: ['@sparticuz/chromium'],
-  },
   images: {
     remotePatterns: [
       {
